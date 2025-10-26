504b4b1d199e74e772585e5d9a3e117f72788b76
Igor-Goncharenko <21gis05ru@gmail.com>
2025-10-26 11:10:00 +0500

    chore: Create initialization

--- cargparse.c
+++ cargparse.c
@@ -1,1 +1,30 @@
 #include "cargparse.h"
+
+#include <stdio.h>
+
+void cargparse_print_help(const cargparse_t *const self) {
+    int i;
+
+    if (self->usages) {
+        const char *const *usages_ptr = self->usages;
+        printf("Usage: %s\n", *usages_ptr++);
+        while (*usages_ptr) {
+            printf("       %s\n", *usages_ptr++);
+        }
+        printf("\n");
+    }
+    if (self->description) {
+        printf("%s\n", self->description);
+        printf("\n");
+    }
+    if (self->n_options > 0) {
+        for (i = 0; i < self->n_options; i++) {
+            printf("  -%c --%s %s\n", self->options[i].short_name, self->options[i].long_name,
+                   self->options[i].help);
+        }
+        printf("\n");
+    }
+    if (self->epilog) {
+        printf("%s\n", self->epilog);
+    }
+}
--- cargparse.h
+++ cargparse.h
@@ -1,5 +1,52 @@
 #ifndef CARGPARSE__H
 #define CARGPARSE__H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    CARGPARSE_OPTION_BOOL = 0,
+    CARGPARSE_OPTION_INT,
+    CARGPARSE_OPTION_STRING,
+} cargparse_option_type_e;
+
+typedef struct {
+    const cargparse_option_type_e type;
+    const char short_name;
+    const char *long_name;
+    const char *help;
+} cargparse_option_t;
+
+typedef struct {
+    const char *const *usages;
+    const char *description;
+    const char *epilog;
+    const cargparse_option_t *options;
+    int n_options;
+} cargparse_t;
+
+#define CARGPARSE_INIT(_usages, _description, _epilog, _options, _n_options) \
+    { \
+        _usages, \
+        _description, \
+        _epilog, \
+        _options, \
+        _n_options, \
+    }
+
+#define CARGPARSE_OPTION_INIT(_type, _short_name, _long_name, _help) \
+    { \
+        _type, \
+        _short_name, \
+        _long_name, \
+        _help, \
+    }
+
+void cargparse_print_help(const cargparse_t *const self);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* CARGPARSE__H */
