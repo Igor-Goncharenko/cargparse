4a55a32e8d3143db72251a02b78245040df5df25
Igor-Goncharenko <21gis05ru@gmail.com>
2025-11-08 14:42:16 +0500

    chore: better bool handling

--- cargparse.c
+++ cargparse.c
@@ -245,6 +245,7 @@ _cargparse_handle_short_option(cargparse_t *const self, const char *arg, int *op
     }
     if (self->options[*opt_idx].type == CARGPARSE_OPTION_TYPE_BOOL) {
         self->parse_res[*opt_idx].is_got = true;
+        self->parse_res[*opt_idx].nargs = 1;
         *opt_idx = -1;
     } else if (self->options[*opt_idx].nargs == CARGPARSE_NARGS_ZERO_OR_MORE) {
         self->parse_res[*opt_idx].is_got = true;
@@ -261,6 +262,7 @@ _cargparse_handle_long_option(cargparse_t *const self, const char *arg, int *opt
     }
     if (self->options[*opt_idx].type == CARGPARSE_OPTION_TYPE_BOOL) {
         self->parse_res[*opt_idx].is_got = true;
+        self->parse_res[*opt_idx].nargs = 1;
         *opt_idx = -1;
     } else if (self->options[*opt_idx].nargs == CARGPARSE_NARGS_ZERO_OR_MORE) {
         self->parse_res[*opt_idx].is_got = true;
@@ -284,6 +286,7 @@ _cargparse_handle_mult_short_bool_options(cargparse_t *const self, const char *a
             return CARGPARSE_ERR_NOT_BOOL_IN_MULT_BOOL_DEF;
         }
         self->parse_res[local_opt_idx].is_got = true;
+        self->parse_res[local_opt_idx].nargs = 1;
     }
 
     return CARGPARSE_OK;
@@ -446,20 +449,15 @@ _cargparse_get_value_generic(const cargparse_t *const self, const cargparse_opti
     if ((ret = _cargparse_validate_input_params(self, long_name, short_name, result)) != CARGPARSE_OK) {
         return ret;
     }
-    int opt_idx = _cargparse_get_check_opt(self, type, short_name, long_name);
+    const int opt_idx = _cargparse_get_check_opt(self, type, short_name, long_name);
     if (opt_idx == -1) {
         return CARGPARSE_ERR_OPTION_UNKNOWN;
     }
-    if (self->options[opt_idx].nargs == CARGPARSE_NARGS_ZERO_OR_MORE && self->parse_res[opt_idx].nargs == 0) {
-        return CARGPARSE_ZERO_NARGS;
-    } else if (self->options[opt_idx].type != CARGPARSE_OPTION_TYPE_BOOL &&
-               narg + 1 > (unsigned)self->parse_res[opt_idx].nargs && self->parse_res[opt_idx].is_got) {
-        return CARGPARSE_ERR_NARG_OUT_OF_RANGE;
-    }
+
     if (!self->parse_res[opt_idx].is_got) {
         switch (type) {
             case CARGPARSE_OPTION_TYPE_BOOL:
-                *(bool *)result = *(const bool *)default_value;
+                *(bool *)result = false;
                 break;
             case CARGPARSE_OPTION_TYPE_INT:
                 *(long *)result = *(const long *)default_value;
@@ -474,22 +472,30 @@ _cargparse_get_value_generic(const cargparse_t *const self, const cargparse_opti
         }
         return CARGPARSE_DEFAULT_VALUE;
     }
-
-    const char *valuestr = *(self->parse_res[opt_idx].valuestr + narg);
+    if (self->options[opt_idx].nargs == CARGPARSE_NARGS_ZERO_OR_MORE && self->parse_res[opt_idx].nargs == 0) {
+        return CARGPARSE_ZERO_NARGS;
+    }
+    if (narg + 1 > (unsigned)self->parse_res[opt_idx].nargs) {
+        return CARGPARSE_ERR_NARG_OUT_OF_RANGE;
+    }
 
     switch (type) {
         case CARGPARSE_OPTION_TYPE_BOOL:
-            *(bool *)result = self->parse_res[opt_idx].is_got;
+            *(bool *)result = true;
             break;
         case CARGPARSE_OPTION_TYPE_INT:
-            if ((ret = _cargparse_parse_int(valuestr, (long *)result)) != CARGPARSE_OK) return ret;
+            if ((ret = _cargparse_parse_int(*(self->parse_res[opt_idx].valuestr + narg), (long *)result)) !=
+                CARGPARSE_OK)
+                return ret;
             break;
         case CARGPARSE_OPTION_TYPE_FLOAT:
-            if ((ret = _cargparse_parse_float(valuestr, (double *)result)) != CARGPARSE_OK) return ret;
+            if ((ret = _cargparse_parse_float(*(self->parse_res[opt_idx].valuestr + narg),
+                                              (double *)result)) != CARGPARSE_OK)
+                return ret;
             break;
         case CARGPARSE_OPTION_TYPE_STR:
         case CARGPARSE_OPTION_TYPE_POS:
-            *(const char **)result = valuestr;
+            *(const char **)result = *(self->parse_res[opt_idx].valuestr + narg);
             break;
     }
 
